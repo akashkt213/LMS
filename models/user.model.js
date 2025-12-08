a206b6b67760746fca0a7af0226601ecff4c5361
Akash <akashkt213@gmail.com>
2025-12-08 23:36:57 +0530

    added role

--- models/user.model.js
+++ models/user.model.js
@@ -15,12 +15,20 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
+  role: {
+    type: Number,
+    enum: [0, 1],
+    default: 1,
+  },
   createdAt: {
     type: Date,
     default: Date.now,
   },
 });
 
+const User = mongoose.model("User", userSchema);
+export default User;
 
-const User = mongoose.model('User',userSchema)
-export default User
+
+// 0 - admin
+// 1 - member
